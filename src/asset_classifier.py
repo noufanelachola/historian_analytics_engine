5c5ef97714e1ce651375db3f386a287fedb38f46
Noufan Elachola <noufansaadiqelachola@gmil.com>
2026-07-31 07:04:27 +0530

    asset profilery v1

---
+++ src/asset_classifier.py
@@ -0,0 +1,50 @@
+import re
+
+asset_types = {
+    "FIT": "Flow Indicator Transmitter",
+    "LIT": "Level Indicator Transmitter",
+    "AIT": "Analyzer Indicator Transmitter",
+    "PIT": "Pressure Indicator Transmitter",
+    "DPIT": "Differential Pressure Indicator Transmitter",
+    "MV": "Motorized Valve",
+    "P": "Pump",
+    "UV": "UV Disinfection Unit"
+}
+
+def classify_asset(asset):
+    asset_info = get_asset_info(asset)
+
+    asset_name = asset_info["prefix"]
+    asset_number = asset_info["number"]
+
+    asset_type = asset_types.get(asset_name, "Unknown Asset Type")
+
+    # The first digit of the asset number indicates the stage of the asset 
+    asset_stage = int(asset_number[0])
+
+    return {
+        "asset": asset,
+        "asset_name": asset_name,
+        "asset_type": asset_type,
+        "asset_number": asset_number,   
+        "asset_stage": asset_stage,
+    }
+
+def get_asset_info(asset):
+    match = re.match(r"([A-Z]+)(\d+)", asset)
+
+    if match:
+        return {
+            "prefix": match.group(1),
+            "number": match.group(2)
+        }
+
+    return None
+
+# def get_asset_number(asset):
+#     match = re.search(r"(\d+)", asset)
+
+#     if match:
+#         return match.group(1)
+
+#     return None
